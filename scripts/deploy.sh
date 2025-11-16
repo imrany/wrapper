110c3c1d53f8d22fb0d079deff8866f76f46c35e
imrany <imranmat254@gmail.com>
2025-11-16 13:17:37 +0300

    chore: updated deploy.sh script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,6 @@ docker stop wrapper 2>/dev/null || true
 docker rm wrapper 2>/dev/null || true
 docker rmi ghcr.io/imrany/wrapper 2>/dev/null || true
 docker pull ghcr.io/imrany/wrapper:latest
-# docker run -d --name wrapper --env-file .env -p 8000:8000 -p 8090:8090  -v ~/.wrapper:/var/opt/wrapper ghcr.io/imrany/wrapper:latest
-docker run -d --name wrapper -p 8000:8000 -p 8090:8090  ghcr.io/imrany/wrapper --port=8000  --api-key=your_key_here  --model=gemini-2.0-flash
+docker run -d --name wrapper --env-file .env -p 8000:8000 -p 8090:8090 -v ~/.wrapper:/var/opt/wrapper ghcr.io/imrany/wrapper:latest
 echo "Deployment complete. Showing logs:"
 docker logs wrapper
